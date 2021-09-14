00a1f3ac6da33b17d6d2b1894e6ab00a677e6d2e
Mobbbb <mobbbb.top@gmail.com>
2021-09-14 15:16:51 +0800

    commit example

---
+++ js/config.js
@@ -0,0 +1,129 @@
+var BASE_COLOR = {
+    DARK_COLOR: '#102154',
+    MIDDLE_COLOR: '#1B3078',
+    LIGHT_COLOR: '#2B4CBA',
+}
+
+var MAP_CONFIG = {
+    '杭州': {
+        color: BASE_COLOR.LIGHT_COLOR,
+        label: true,
+        value: 0.5,
+        symbolConfig: {
+            x: 119.443576,
+            y: 30.007459,
+            url: "image://./images/pos.png",
+            size: [30, 40],
+        },
+    },
+    '温州': {
+        color: BASE_COLOR.MIDDLE_COLOR,
+        label: true,
+        value: 0.5,
+        symbolConfig: {
+            x: 120.572111,
+            y: 28.250575,
+            url: "image://./images/pos.png",
+            size: [30, 40],
+        },
+    },
+    '宁波': {
+        color: BASE_COLOR.LIGHT_COLOR,
+        label: true,
+        value: 0.5,
+        symbolConfig: {
+            x: 121.409792,
+            y: 29.848388,
+            url: "image://./images/pos.png",
+            size: [30, 40],
+        },
+    },
+    '湖州': {
+        color: BASE_COLOR.DARK_COLOR,
+    },
+    '嘉兴': {
+        color: BASE_COLOR.MIDDLE_COLOR,
+        label: true,
+        value: 0.5,
+        symbolConfig: {
+            x: 120.830865,
+            y: 30.922653,
+            url: "image://./images/pos.png",
+            size: [30, 40],
+        },
+    },
+    '金华': {
+        color: BASE_COLOR.LIGHT_COLOR,
+        label: true,
+        value: 0.5,
+        symbolConfig: {
+            x: 119.889506,
+            y: 29.389524,
+            url: "image://./images/pos.png",
+            size: [30, 40],
+        },
+    },
+    '丽水': {
+        color: BASE_COLOR.LIGHT_COLOR,
+        label: true,
+        value: 0.5,
+        symbolConfig: {
+            x: 119.441786,
+            y: 28.401993,
+            url: "image://./images/pos.png",
+            size: [30, 40],
+        },
+    },
+    '衢州': {
+        color: BASE_COLOR.DARK_COLOR,
+    },
+    '绍兴': {
+        color: BASE_COLOR.MIDDLE_COLOR,
+    },
+    '台州': {
+        color: BASE_COLOR.MIDDLE_COLOR,
+        label: true,
+        value: 0.5,
+        symbolConfig: {
+            x: 121.278599,
+            y: 28.991378,
+            url: "image://./images/pos.png",
+            size: [30, 40],
+        },
+    },
+    '上海': {
+        color: BASE_COLOR.LIGHT_COLOR,
+        label: true,
+        value: 0.5,
+        symbolConfig: {
+            x: 121.503576,
+            y: 31.357459,
+            url: "image://./images/pos.png",
+            size: [30, 40],
+        },
+    },
+    '福州': {
+        color: BASE_COLOR.LIGHT_COLOR,
+        label: true,
+        labelPosition: [-6, 2],
+        value: 0.5,
+        symbolConfig: {
+            x: 117.30,
+            y: 28.00,
+            url: "image://./images/pos.png",
+            size: [30, 40],
+        },
+    },
+    '南京': {
+        color: BASE_COLOR.LIGHT_COLOR,
+        label: true,
+        labelPosition: [5, 18],
+        value: 0.5,
+        symbolConfig: {
+            x: 117.39,
+            y: 31.480459,
+            url: "image://./images/pos.png",
+            size: [30, 40],
+        },
+    },
+}
